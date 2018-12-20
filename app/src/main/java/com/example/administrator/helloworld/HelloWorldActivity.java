87b415716840714ce08eb38971a56233732c32c1
rumengsuifeng <niguanghuisz@163.com>
2018-12-20 18:50:54 +0800

    硬件服务的LED-APP，已经运用了服务框架

--- app/src/main/java/com/example/administrator/helloworld/HelloWorldActivity.java
+++ app/src/main/java/com/example/administrator/helloworld/HelloWorldActivity.java
@@ -2,6 +2,7 @@ package com.example.administrator.helloworld;
 
 import android.os.Bundle;
 import android.os.Handler;
+
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -12,7 +13,9 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.Toast;
-import com.example.administrator.hardware.*;
+
+import android.os.ILedService;
+import android.os.ServiceManager;
 
 public class HelloWorldActivity extends AppCompatActivity {
     private Button button = null;       //定义按键
@@ -20,13 +23,15 @@ public class HelloWorldActivity extends AppCompatActivity {
     private CheckBox checkBoxled1 = null;   //定义复选框
     private CheckBox checkBoxled2 = null;
     private CheckBox Beep = null;
+    private ILedService iLedService = null;      //定义调用APP接口的对象
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);                 //调用父类onCreate方法
         setContentView(R.layout.activity_hello_world);      //给当前活动加载一个布局
 
-        HardCor.ledOpen();      //静态方法调用，先是打开LED
+        iLedService = ILedService.Stub.asInterface(ServiceManager.getService("led"));   //拿到系统提供的服务接口
+        //iLedService.ledOpen();      //静态方法调用，先是打开LED
         button = (Button) findViewById(R.id.button);        //从布局文件中拿到按键ID
 
         checkBoxled1 = (CheckBox) findViewById(R.id.LED1);  //从布局文件中拿到复选框的ID
@@ -37,7 +42,7 @@ public class HelloWorldActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // Code here executes on main thread after user presses button
 
-//                HardCor hardCor = new HardCor();    //会调用到HardCor.java中的static块
+//                iLedService iLedService = new iLedService();    //会调用到iLedService.java中的static块
 
                 ledon = !ledon;
                 if (ledon) {
@@ -46,17 +51,25 @@ public class HelloWorldActivity extends AppCompatActivity {
                     checkBoxled2.setChecked(true);
                     Beep.setChecked(true);
 
-                    HardCor.ledCtrl(364, 1);
-                    HardCor.ledCtrl(362, 1);
-                    HardCor.ledCtrl(120, 1);
+                    try {
+                        iLedService.ledCtrl(364, 1);    //调用服务接口的静态方法
+                        iLedService.ledCtrl(362, 1);
+                        iLedService.ledCtrl(120, 1);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 } else {
                     button.setText("ALL OFF");          //更改按键上的显示字符
                     checkBoxled1.setChecked(false);     //设置复选框没选中
                     checkBoxled2.setChecked(false);
                     Beep.setChecked(false);
-                    HardCor.ledCtrl(364, 0);
-                    HardCor.ledCtrl(362, 0);
-                    HardCor.ledCtrl(120, 0);
+                    try {
+                        iLedService.ledCtrl(364, 0);
+                        iLedService.ledCtrl(362, 0);
+                        iLedService.ledCtrl(120, 0);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         });
@@ -106,31 +119,55 @@ public class HelloWorldActivity extends AppCompatActivity {
                 if (checked)    //如果是选中
                 {
                     Toast.makeText(HelloWorldActivity.this, "LED1 ON", Toast.LENGTH_SHORT).show();      //打印字符串到屏幕上
-                HardCor.ledCtrl(364, 1);      //控制LED灯的亮灭
-        }
+                    try {
+                        iLedService.ledCtrl(364, 1);      ////调用服务接口的静态方法，控制LED灯的亮灭
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
             else{
             Toast.makeText(HelloWorldActivity.this, "LED1 OFF", Toast.LENGTH_SHORT).show();
-            HardCor.ledCtrl(364, 0);
-        }
+                    try {
+                        iLedService.ledCtrl(364, 0);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
                 break;
             case R.id.LED2:             //LED的驱动标号计算公式是(x * 32) + 10, x是A的时候就是0，x是B的时候就是1，依次类推
                 if (checked) {              //本例子用的是L，所以是11 * 32; 10就是后边跟的标号，加上10所以就是362
                     Toast.makeText(HelloWorldActivity.this, "LED2 ON", Toast.LENGTH_SHORT).show();
-                    HardCor.ledCtrl(362, 1);
+                    try {
+                        iLedService.ledCtrl(362, 1);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
             else{
                     Toast.makeText(HelloWorldActivity.this, "LED2 OFF", Toast.LENGTH_SHORT).show();
-                    HardCor.ledCtrl(362, 0);
+                    try {
+                        iLedService.ledCtrl(362, 0);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
                 break;
             case R.id.BEEP:
                 if (checked) {
                     Toast.makeText(HelloWorldActivity.this, "BEEP ON", Toast.LENGTH_SHORT).show();
-                    HardCor.ledCtrl(120, 1);
+                    try {
+                        iLedService.ledCtrl(120, 1);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
                 else {
                     Toast.makeText(HelloWorldActivity.this, "BEEP OFF", Toast.LENGTH_SHORT).show();
-                    HardCor.ledCtrl(120, 0);
+                    try {
+                        iLedService.ledCtrl(120, 0);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
                 break;
             // TODO: Veggie sandwich
